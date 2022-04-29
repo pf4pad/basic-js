43f3ddd99348a377a52a1253a75ae7ed1b74ec80
Pavlo Cherevach <pf4pad.fr@gmail.com>
2022-04-29 14:43:51 +0300

    rs-school

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+
+  let numbers = arr.filter((el) => el != -1);
+  numbers.sort((a, b) => b - a);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== -1) {
+      arr[i] = numbers.pop();
+    }
+  }
+  return arr;
 }
 
 module.exports = {
